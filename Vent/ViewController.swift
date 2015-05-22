5f9b5507c92929ae33657625aa6204813da2c5a2
dodo-mac <dodo-mac@dodo-macs-Mac.local>
2015-05-22 13:42:04 +0200

    Initial Commit

---
+++ Vent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vent
+//
+//  Created by dodo-mac on 22/05/2015.
+//  Copyright (c) 2015 dodo-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
